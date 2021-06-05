f4e0edc4bba86fbe918e26f4dc04acba8d97d350
mink21 <minkyoung2112@gmail.com>
2021-06-05 12:41:39 +0900

    christmas clock

--- src/clcok.js
+++ src/clcok.js
@@ -1,14 +1,22 @@
-const clockContainer = document.querySelector(".js-clock")
-const clockTitle = clockContainer.querySelector("h1");
+import "./styles.css";
 
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h2");
+
+// You're gonna need this
 function getTime() {
+    // Don't delete this.
+    const xmasDay = new Date("2021-12-24:00:00:00+0900");
     const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}
-    :${minutes < 10 ? `0${minutes}` : minutes}
-    :${seconds < 10 ? `0${seconds}` : seconds}`;
+    let diff = (xmasDay - date) / 1000;
+    const days = Math.ceil(diff / (60 * 60 * 24)) - 1;
+    diff = diff - days * 60 * 60 * 24;
+    const hours = Math.ceil(diff / (60 * 60)) - 1;
+    diff = diff - hours * 60 * 60;
+    const minutes = Math.ceil(diff / 60) - 1;
+    diff = diff - minutes * 60;
+    const seconds = Math.ceil(diff);
+    clockTitle.innerText = `${days < 10 ? `0${days}` : days}d ${hours < 10 ? `0${hours}` : hours}h ${minutes < 10 ? `0${minutes}` : minutes}m ${seconds < 10 ? `0${seconds}` : seconds}s`;
 }
 
 function init() {
